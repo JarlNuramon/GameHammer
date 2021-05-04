c0a6277560cf1aca99f5c7568f18c3365efda8ea
JarlNuramon <saiedsadeghz@gmail.com>
2021-05-04 09:55:49 +0200

    I did the thing again

--- src/Component/GameView/GameTurn.jsx
+++ src/Component/GameView/GameTurn.jsx
@@ -1,7 +1,29 @@
-export default function GameTurn() {
+import { Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+export default function GameTurn(props) {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
   return (
     <div className="gameTurn">
-     
+      <center>
+        <h1>{props.phase}</h1>
+        <Button disabled={isLoading} onClick={!isLoading ? handleClick : null}>
+          {isLoading ? "Loading…" : "Next phase"}
+        </Button>
+      </center>
     </div>
   );
+}
+
+function simulateNetworkRequest() {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
 }
--- src/Component/GameView/GameView.jsx
+++ src/Component/GameView/GameView.jsx
@@ -11,19 +11,21 @@ export default function GameView() {
         <div className="colu-5 colu-s-5">
           <PlayerComponent name="player1" side="left" />
         </div>
-        <div className="colu-2 colu-s-5">
+        <div className="colu-2 colu-s-2">
           <h2>
             <center>
               <h2>vs</h2>
             </center>
           </h2>
         </div>
-        <div className="colu-5 colu-s-4">
+        <div className="colu-5 colu-s-5">
           <PlayerComponent name="player2" side="right" />
         </div>
       </div>
       <div className="row">
-        <GameTurn />
+        <div className="colu-12 colu-s-12">
+          <GameTurn phase={"phasedummy"} />
+        </div>
       </div>
     </div>
   );
