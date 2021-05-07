1f834dc27c23eff174e44d85ac33147a5ac38dd0
JarlNuramon <saiedsadeghz@gmail.com>
2021-05-08 00:32:58 +0200

    responsive fixes

--- src/Component/DashboardView/Dashboard.jsx
+++ src/Component/DashboardView/Dashboard.jsx
@@ -1,17 +1,23 @@
 import Sidewinder from "./Sidewinder";
 import GameTable from "./GameTable";
+import ButtonRow from "./ButtonRow";
 import { useParams, Redirect } from "react-router-dom";
 
 export default function Header() {
   let { id } = useParams();
   if (id)
     return (
-      <div className="row">
-        <div className="colu-2 colu-s-4">
-          <Sidewinder />
+      <div>
+        <div className="row">
+          <div className="colu-2 colu-s-4">
+            <Sidewinder />
+          </div>
+          <div className="colu-6 colu-s-6">
+            <GameTable />
+          </div>
         </div>
-        <div className="colu-6 colu-s-6">
-          <GameTable />
+        <div className="row">
+          <ButtonRow />
         </div>
       </div>
     );
