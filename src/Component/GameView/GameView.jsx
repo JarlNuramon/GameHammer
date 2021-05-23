2185d3cd39902607b7b7d68af8229e57289789a8
JarlNuramon <saiedsadeghz@gmail.com>
2021-05-23 10:26:48 +0200

    Game View Phase

--- src/Component/GameView/GameView.jsx
+++ src/Component/GameView/GameView.jsx
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 import PlayerComponent from "./PlayerComponent";
 import GameTurn from "./GameTurn";
+import { useState, useEffect } from "react";
 export default function GameView() {
   let { id, gameid } = useParams();
   console.log("id: ", id, " gameid:", gameid);
+  const [getPhase, setPhase] = useState(1);
+  const [getTurn, setTurn] = useState(1);
+  const map = {
+    1: "Command",
+    2: "Psychic",
+    3: "Move",
+    4: "Shooting",
+    5: "Charge",
+    6: "Assault",
+    0: "Moral"
+  };
 
   return (
     <div>
@@ -17,7 +29,7 @@ export default function GameView() {
       </div>
       <div className="row">
         <div className="colu-12 colu-s-12">
-          <GameTurn phase={"phasedummy"} />
+          <GameTurn phase={map(getPhase() % 7)} />
         </div>
       </div>
     </div>
