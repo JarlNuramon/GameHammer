8aea60eb634c59ee605ce3d948a856f81e6f8c0e
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2021-06-16 10:18:00 +0200

    fixing path for game/user/start
    Bug: User can still load game/"Any other user"/start

--- src/Component/DashboardView/ButtonRow.jsx
+++ src/Component/DashboardView/ButtonRow.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import cookie from "react-cookies";
 
 export default function ButtonRow() {
+  let user = cookie.load("userIdentifier");
   return (
     <div className="buttonrow">
       <ul>
         <li>
-          <Link to={`/game/a/start`}> Start new Game</Link>
+          <Link to={`/game/${user}/start`}> Start new Game</Link>
         </li>
         <li>
-          <Link to={`/game/a/start`}>Configure Army Lists</Link>
+          <Link to={`/game/${user}/start`}>Configure Army Lists</Link>
         </li>
         <li>
           <Link to={`/logout`}> Logout</Link>
--- src/Component/DashboardView/Sidewinder.jsx
+++ src/Component/DashboardView/Sidewinder.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Link, useRouteMatch } from "react-router-dom";
+import cookie from "react-cookies";
 
 export default function Sidewinder() {
+  let user = cookie.load("userIdentifier");
   return (
     <div className="menu">
       <ul>
         <li>
-          <Link to={`/game/a/start`}> Start new Game</Link>
+          <Link to={`/game/${user}/start`}> Start new Game</Link>
         </li>
         <li>
-          <Link to={`/game/a/start`}> Configure Army Lists</Link>
+          <Link to={`/game/${user}/start`}> Configure Army Lists</Link>
         </li>
         <li>
           <Link to={`/logout`}> Logout</Link>
