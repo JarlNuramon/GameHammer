306243bf25fe039aef168a6ecfac98095edd06c0
JarlNuramon <saiedsadeghz@gmail.com>
2021-06-17 22:32:06 +0200

    i tried

---
+++ src/themes.js
@@ -0,0 +1,26 @@
+const themes = {
+  none: {
+    primary: "#fff",
+    secondary: "grey",
+    tertiary: "whitesmoke",
+    contrast: "black",
+    powerless: "rgb(66, 55, 55)",
+    minus: "red",
+    plus: "green"
+  },
+  Ultramarines: {
+    // primary: "rgb(22, 22, 22)",
+    //secondary: "rgb(22, 0, 100)",
+    //tertiary: "#002281",
+    //contrast: "rgb(179, 152, 0)",
+    //powerless: "rgb(16, 80, 255)",
+    //minus: "gold",
+    // plus: "gold",
+    image: 'url("/Marines600x600.png")'
+  },
+  Necrons: {
+    image: 'url("/necron600x600.png")'
+  }
+};
+
+export default themes;
